d5a358b596bd4214009a482e7ac88d4381af0c6a
ProfDave <dave.lacourse@cegeptr.qc.ca>
2025-03-05 17:08:12 -0500

    fin

--- Assets/_MyAssets/Scripts/TrapsManager.cs
+++ Assets/_MyAssets/Scripts/TrapsManager.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TrapsManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _piege = default(GameObject);
+    [SerializeField] private List<GameObject> _listePieges = new List<GameObject>();
     [SerializeField] private float _intensity = 500;
     
     [Header("Direction du vecteur de chute")]
@@ -10,20 +11,33 @@ public class TrapsManager : MonoBehaviour
     [SerializeField] private float _directionY = -1;
     [SerializeField] private float _directionZ = 0;
 
-    private Rigidbody _rb;
+    private List<Rigidbody> _listeRb = new List<Rigidbody>();
     private Vector3 _direction;
+    private bool _isTrigger = false;
 
     private void Start()
     {
-        _rb = _piege.GetComponent<Rigidbody>();
-        _rb.useGravity= false;
+        foreach(var piege in _listePieges)
+        {
+            _listeRb.Add(piege.GetComponent<Rigidbody>());
+            piege.GetComponent<Rigidbody>().useGravity = false;
+        }
         _direction = new Vector3(_directionX, _directionY, _directionZ);
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        _rb.useGravity = true;
-        _rb.AddForce(_direction * _intensity);
+        
+        if(other.tag == "Player" && !_isTrigger)
+        {
+            foreach(var rb in _listeRb)
+            {
+                rb.gameObject.SetActive(true);
+                rb.useGravity = true;
+                rb.AddForce(_direction * _intensity);
+            }
+            _isTrigger = true;
+        }
     }
 }
